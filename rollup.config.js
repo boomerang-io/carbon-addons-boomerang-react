26020b6695ed75a79d322f079d97ca38351c76b4
Lucas Grimauth Evangelista <Lucas.Grimauth@ibm.com>
2020-10-23 15:08:30 -0300

    fix: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -33,6 +33,7 @@ const baseConfig = {
       include: './node_modules/**',
       namedExports: {
         '@stomp/stompjs/bundles/stomp.umd.js': ['Client'],
+        'react-is/index.js': ['isForwardRef'],
         'carbon-components-react/lib/components/ComposedModal/index.js': [
           'ModalHeader',
           'ModalBody',
