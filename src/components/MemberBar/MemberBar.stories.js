9d3d9ddfa9e066fb3fdf64cbfd33c098cf667e73
Isabela Kawabata <isabela.kawabata@ibm.com>
2021-02-04 16:13:01 -0300

    feat: add new member bar type - user not allowed

--- src/components/MemberBar/MemberBar.stories.js
+++ src/components/MemberBar/MemberBar.stories.js
@@ -8,7 +8,7 @@ storiesOf('MemberBar', module).add('default', () => {
       <MemberBar name="Panda Black White" email="panda@email.com" isPartner />
       <MemberBar name="Mico Leao Dourado" email="mico@email.com" removeUser />
       <MemberBar name="Coruja Owl" email="owl@email.com" isPartner removeUser />
-      <MemberBar name="Gato Neko" email="neko@email.com" />
+      <MemberBar name="Gato Neko" email="neko@email.com" isUserNotAllowed addUser={() => console.log("yay")}/>
     </ul>
   );
 });
