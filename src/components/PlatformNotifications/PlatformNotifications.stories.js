cbf50c836d9aa4e9e66105818ea77ec477afc4c8
Tim R. Bula <timrbula@gmail.com>
2022-03-21 19:56:59 -0500

    feat: fix platform notifications date parsing

--- src/components/PlatformNotifications/PlatformNotifications.stories.js
+++ src/components/PlatformNotifications/PlatformNotifications.stories.js
@@ -5,24 +5,31 @@ import { boolean } from '@storybook/addon-knobs';
 import PlatformNotificationsContainer from './index';
 
 // const mockSocketUrl = 'http://localhost:7750/notifications/ws';
-const mockSocketUrl = 'https://www.google.com/notifications/ws';
 
 export default {
   title: 'PlatformNotifications',
 };
+
+const mockSocketUrl = 'http://localhost:8080/ws';
 
 export const Default = () => {
   return (
     <PlatformNotificationsContainer
       initialNotifications={[
         {
-          id: 'testId',
-          creator: 'launchpad',
-          title: 'Maintenance Scheduled for a long long long long time',
+          creator: 'Boomerang CICD',
+          date: '2022-03-15T12:47:47.655+00:00',
           detail:
-            'Launchpad is scheduled for maintenance on January 8 from 2am-3am for a long long long time',
-          date: 1555079172156,
-          type: 'exception',
+            'Outage description test for the following service(s): Boomerang Flow,Boomerang CICD',
+          eventId: '620b9f7e99fcb715cbc222b3',
+          id: '620ba0f354f1b83f5077dec6',
+          priority: 'highest',
+          read: false,
+          severity: 'INFO',
+          target: 'user',
+          title: 'Resolved',
+          type: 'notification',
+          userId: '61730018ae92414d2bd15b4c',
         },
       ]}
       config={{
