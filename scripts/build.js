cc2809f8fabc5cd13d30fe3add07eeb8cce19109
Tim R. Bula <timrbula@gmail.com>
2022-03-30 13:30:58 -0500

    fix: cross platform build

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const { execSync } = require('child_process');
+const path = require('path');
 const { inInstall } = require('in-publish');
 const which = require('npm-which')(__dirname);
+const fse = require('fs-extra');
+const cpy = require('cpy');
 
 if (inInstall()) {
   process.exit(0);
@@ -18,16 +21,22 @@ const exec = (command, extraEnv) =>
 
 const ignoreGlobs = ['**/*.spec.js', '**/*.stories.js'].join(',');
 
-try {
-  exec(`${babelPath} src --quiet -d es --ignore "${ignoreGlobs}"`, {
-    BABEL_ENV: 'es',
-  });
-  exec(`${babelPath} src --quiet -d lib --ignore "${ignoreGlobs}"`, {
-    BABEL_ENV: 'cjs',
-  });
-  exec(`mkdir -p styles/scss`);
-  exec(`rsync -avm --include='*.scss' -f 'hide,! */' ./src/* ./styles/scss`);
-} catch (error) {
-  console.error('One of the commands failed:', error.stack); // eslint-disable-line no-console
-  process.exit(1);
+async function build() {
+  try {
+    // exec(`${babelPath} src --quiet -d es --ignore "${ignoreGlobs}"`, {
+    //   BABEL_ENV: 'es',
+    // });
+    // exec(`${babelPath} src --quiet -d lib --ignore "${ignoreGlobs}"`, {
+    //   BABEL_ENV: 'cjs',
+    // });
+    fse.ensureDirSync('styles/scss');
+    await cpy('src/**/*.scss', 'styles/scss', { parents: true });
+  } catch (error) {
+    console.error('One of the commands failed:', error.stack); // eslint-disable-line no-console
+    process.exit(1);
+  }
 }
+
+(async (args) => {
+  await build(args);
+})(process.argv);
