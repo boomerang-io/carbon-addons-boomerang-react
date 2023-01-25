656a061176270f51ac338d3c8e8ec07002fd0d5b
Tim R. Bula <trbula@us.ibm.com>
2023-01-25 10:10:09 -0600

    chore: remove unused import

--- src/components/AutoSuggest/AutoSuggest.spec.tsx
+++ src/components/AutoSuggest/AutoSuggest.spec.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { expect, test } from "vitest";
-import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { axe } from "jest-axe";
 
