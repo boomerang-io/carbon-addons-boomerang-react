78ec720d77b55b7db5f1dadf432844ae64afb81c
Lucas Grimauth Evangelista <Lucas.Grimauth@ibm.com>
2024-05-23 18:22:18 -0300

    feat: upgrade vite v5

--- src/tools/useSetState.ts
+++ src/tools/useSetState.ts
@@ -5,7 +5,7 @@ const useSetState = <T extends object>(
   initialState: T = {} as T
 ): [T, (patch: Partial<T> | ((prevState: T) => Partial<T>)) => void] => {
   const [state, set] = useState<T>(initialState);
-  const setState = useCallback((patch) => {
+  const setState = useCallback((patch: any) => {
     set((prevState) => Object.assign({}, prevState, patch instanceof Function ? patch(prevState) : patch));
   }, []);
 
