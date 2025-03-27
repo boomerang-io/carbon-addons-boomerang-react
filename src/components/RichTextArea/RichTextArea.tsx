87b69ec227cc546fa4582c731b79d25dc1cf5da5
Isabela Mayumi Kawabata Borges <Isabela.Kawabata@ibm.com>
2025-03-27 12:37:42 -0300

    code cleanup and new version

--- src/components/RichTextArea/RichTextArea.tsx
+++ src/components/RichTextArea/RichTextArea.tsx
@@ -4,8 +4,7 @@ IBM Confidential
 © Copyright IBM Corp. 2022, 2025
 */
 
-import React, { lazy, useEffect, useRef, useState } from "react";
-// import Quill from "quill";
+import React, { useEffect, useRef, useState } from "react";
 import { Toolbar, ToolbarButton, ToolbarGroup, pkg } from "@carbon/ibm-products";
 import "quill/dist/quill.snow.css";
 import { prefix } from "../../internal/settings";
@@ -28,20 +27,6 @@ type Props = React.ComponentPropsWithRef<"input"> & {
   quillProps?: any;
 };
 
-// (async () => {
-//   const {default: Quill} = await import("quill");
-//   let qi = new Quill();
-//   // ...
-// })()
-// .catch(error => {
-//   // Handle/report error
-//   console.error(error);
-// });
-
-// const Quill = lazy(async () => await import(/* webpackChunkName: "Quill" */ "quill").then(module => {
-//   console.log("MODULEE", module);
-//   return { default: module.Quill };
-// }));
 const RichTextAreaComponent = React.forwardRef<any, Props>(function RichTextAreaComponent(
   {
     label,
@@ -114,45 +99,6 @@ const RichTextAreaComponent = React.forwardRef<any, Props>(function RichTextArea
     .catch(error => {
       console.error(error);
     });
-    
-    // if (editorRef.current) {
-    //   const quill = new Quill(editorRef.current, {
-    //     theme: "snow",
-    //     modules: {
-    //       toolbar: false,
-    //     },
-    //     placeholder: !readOnly ? placeholder : "",
-    //     readOnly,
-    //     ...quillProps,
-    //   });
-    //   quillRef.current = quill;
-    //   if (value) {
-    //     const cleanHtml = sanitizeHtml(value, {
-    //       allowedTags: ["p", "strong", "b", "i", "em", "u", "ol", "ul", "li", "a"],
-    //       allowedAttributes: {
-    //         a: ["href", "rel", "target"],
-    //       },
-    //     });
-    //     quillRef.current.clipboard.dangerouslyPasteHTML(cleanHtml);
-    //     const length = quillRef.current.getLength();
-    //     quillRef.current.setSelection(length, 0);
-    //     setWordCount(getWordCount());
-    //     if (setError) {
-    //       setError(maxWordCount && getWordCount() > maxWordCount);
-    //     }
-    //   }
-    //   quill.on("text-change", () => {
-    //     const currentCount = getWordCount();
-    //     setWordCount(currentCount);
-    //     const wordCountExceeded = maxWordCount && currentCount > maxWordCount;
-    //     if (onChange && !wordCountExceeded) {
-    //       onChange(quill.getSemanticHTML());
-    //     }
-    //     if (setError) {
-    //       setError(wordCountExceeded);
-    //     }
-    //   });
-    // }
   }, []);
 
   const handleBold = () => {
