ffd8b21ca59ae8029a979d03617d60f3416b05ac
Lucas Grimauth Evangelista <Lucas.Grimauth@ibm.com>
2025-04-03 09:30:37 -0300

    feat: [ICA1-837] about platform copyright

--- src/components/AboutPlatform/AboutPlatform.tsx
+++ src/components/AboutPlatform/AboutPlatform.tsx
@@ -1,7 +1,7 @@
 /*
 IBM Confidential
 694970X, 69497O0
-© Copyright IBM Corp. 2022, 2024
+© Copyright IBM Corp. 2022, 2025
 */
 
 
@@ -9,15 +9,7 @@ import React from "react";
 import { ComposedModal, ModalHeader, ModalBody } from "@carbon/react";
 import { Information } from "@carbon/react/icons";
 import HeaderMenuItem from "../Header/HeaderMenuItem";
-import IBMCloudIcon from "./assets/IBMCloudIcon";
-import KubernetesIcon from "./assets/KubernetesIcon";
-import MongoDbIcon from "./assets/MongoDbIcon";
-import OpenShiftIcon from "./assets/OpenShiftIcon";
-import ReactIcon from "./assets/ReactIcon";
-import SpringIcon from "./assets/SpringIcon";
 import { prefix } from "../../internal/settings";
-
-const iconClassName = `${prefix}--bmrg-aboutPlatform-images__img`;
 
 type Props = {
   closeModal: () => void;
@@ -36,44 +28,7 @@ function AboutPlatform({ closeModal, isOpen = false, version, name }: Props) {
       <ModalHeader label={`${name}  |  Version ${version}`} title="About the Platform" closeModal={closeModal} />
       <ModalBody>
         <footer className={`${prefix}--bmrg-aboutPlatform-footer`}>
-          <h1 className={`${prefix}--bmrg-aboutPlatform-footer__header`}>Powered by</h1>
-          <ul className={`${prefix}--bmrg-aboutPlatform-images`}>
-            <li key="ibm-cloud-icon">
-              <a href="https://www.ibm.com/cloud" target="_blank" rel="noopener noreferrer" title="IBM Cloud">
-                <IBMCloudIcon className={iconClassName} />
-              </a>
-            </li>
-            <li key="openshift-icon">
-              <a
-                href="https://www.redhat.com/en/technologies/cloud-computing/openshift"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="OpenShift"
-              >
-                <OpenShiftIcon className={iconClassName} />
-              </a>
-            </li>
-            <li key="kubernetes-icon">
-              <a href="https://kubernetes.io/" target="_blank" rel="noopener noreferrer" title="Kubernetes">
-                <KubernetesIcon className={iconClassName} />
-              </a>
-            </li>
-            <li key="spring-icon">
-              <a href="https://spring.io/" target="_blank" rel="noopener noreferrer" title="Spring">
-                <SpringIcon className={iconClassName} />
-              </a>
-            </li>
-            <li key="react-icon">
-              <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer" title="React">
-                <ReactIcon className={iconClassName} />
-              </a>
-            </li>
-            <li key="mongodb-icon">
-              <a href="https://www.mongodb.com/" target="_blank" rel="noopener noreferrer" title="MongoDB">
-                <MongoDbIcon className={iconClassName} />
-              </a>
-            </li>
-          </ul>
+          <h1 className={`${prefix}--bmrg-aboutPlatform-footer__header`}>Copyright IBM Corp. 2022, 2025</h1>
         </footer>
       </ModalBody>
     </ComposedModal>
