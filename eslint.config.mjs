4af65b71785f7325b10ea5d148407bc7f77bae8a
Lucas Grimauth Evangelista <lucas.grimauth@Lucass-MacBook-Pro.local>
2026-04-06 11:43:19 -0300

    fix: vulnerabilities, storybook 10 migration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -7,62 +10,57 @@ import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
 
-export default [
-  {
-    ignores: [
-      "node_modules",
-      "coverage",
-      "config",
-      "lib",
-      "docs",
-      "storybook-static",
-      "results",
-      "rollup.config.mjs"
-    ]
+export default [{
+  ignores: [
+    "node_modules",
+    "coverage",
+    "config",
+    "lib",
+    "docs",
+    "storybook-static",
+    "results",
+    "rollup.config.mjs"
+  ]
+}, js.configs.recommended, {
+  files: ["src/**/*.{js,jsx,ts,tsx}"],
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      ecmaFeatures: { jsx: true }
+    },
+    globals: {
+      ...globals.browser,
+      ...globals.vitest,
+      ...globals.node
+    },
   },
-  js.configs.recommended,
-  {
-    files: ["src/**/*.{js,jsx,ts,tsx}"],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaFeatures: { jsx: true }
-      },
-      globals: {
-        ...globals.browser,
-        ...globals.vitest,
-        ...globals.node
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-      react: react,
-      "react-hooks": reactHooks,
-      "jsx-a11y": jsxA11y,
-      "testing-library": testingLibrary
-    },
-    settings: {
-      react: {
-        version: "detect"
-      }
-    },
-    rules: {
-      "no-unused-vars": "warn",
-      "no-extra-boolean-cast": "warn",
-      "no-constant-binary-expression": "warn",
-      "preserve-caught-error": "warn",
-      "no-useless-assignment": "warn",
+  plugins: {
+    "@typescript-eslint": tsPlugin,
+    react: react,
+    "react-hooks": reactHooks,
+    "jsx-a11y": jsxA11y,
+    "testing-library": testingLibrary
+  },
+  settings: {
+    react: {
+      version: "detect"
     }
   },
-  {
-    files: ["**/.stories."],
-    rules: {
-      "import/no-anonymous-default-export": 0,
-      "no-unused-vars": "warn",
-      "no-extra-boolean-cast": "warn",
-      "no-constant-binary-expression": "warn",
-      "preserve-caught-error": "warn",
-      "no-useless-assignment": "warn",
-    }
+  rules: {
+    "no-unused-vars": "warn",
+    "no-extra-boolean-cast": "warn",
+    "no-constant-binary-expression": "warn",
+    "preserve-caught-error": "warn",
+    "no-useless-assignment": "warn",
   }
-];
+}, {
+  files: ["**/.stories."],
+  rules: {
+    "import/no-anonymous-default-export": 0,
+    "no-unused-vars": "warn",
+    "no-extra-boolean-cast": "warn",
+    "no-constant-binary-expression": "warn",
+    "preserve-caught-error": "warn",
+    "no-useless-assignment": "warn",
+  }
+}, ...storybook.configs["flat/recommended"]];
